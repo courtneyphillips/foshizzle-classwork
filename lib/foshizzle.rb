9632d3b75da3b6b88b502725cf6f68426e56047b
Courtney <courtney.mae.phillips@gmail.com>
2015-07-30 16:33:17 -0700

    Add code to prevent first letter from transforming. All tests pass.

--- lib/foshizzle.rb
+++ lib/foshizzle.rb
@@ -1,17 +1,19 @@
 class String
-
   define_method(:foshizzle) do
-    split_word = self.split("")
-    new_word = []
-    split_word.each do |letter|
-      if letter == "s"
-        new_word.push("z")
-      else
-        new_word.push(letter)
+    words = split(" ")
+    new_words = []
+    words.each do |word|
+      letters = word.split('')
+      new_letters = [letters.shift()]
+      letters.each() do |letter|
+        if letter == "s"
+          new_letters.push("z")
+        else
+          new_letters.push(letter)
+        end
+      end
+      new_words.push(new_letters.join(''))
     end
+    new_words.join(' ')
   end
-    new_word.join()
-  end
-
-
 end
--- spec/foshizzle_spec.rb
+++ spec/foshizzle_spec.rb
@@ -4,8 +4,18 @@ require('rspec')
 describe('String#foshizzle') do
 
   it('will replace s with z in a single word') do
-    expect("salad".foshizzle()).to(eq("zalad"))
+    expect("salads".foshizzle()).to(eq("saladz"))
   end
 
+  it('will replace s with z in a phrase') do
+    expect("the salads are so salty".foshizzle()).to(eq("the saladz are so salty"))
+  end
 
+  it('will not replace a capital s with z') do
+    expect("Susan is so salty".foshizzle()).to(eq("Suzan iz so salty"))
+  end
+
+  it('will not replace the first letter of a word with z') do
+    expect("Susan is so salty".foshizzle()).to(eq("Suzan iz so salty"))
+  end
 end
